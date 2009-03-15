ae433f67ffccd7f93629e1f5bc9707cd7132b352
domedamian <domedamian@b83fa03a-0f66-11de-b559-cb2af582aa9e>
2009-03-15 17:57:28 +0000

    Manejo de files

---
+++ trunk/defines.h
@@ -0,0 +1,19 @@
+/*
+ *  defines.h
+ *  Usen este header para todos sus defines, typedef y enum
+ *
+ *  Created by Damian Dome on 3/12/09.
+ *  
+ *  
+ */
+
+#ifndef _DEFINES_
+#define _DEFINES_
+
+enum  {ERROR = -1, OK = 1};
+
+typedef enum boolean{ FALSE = 0, TRUE = 1} boolean;
+
+typedef char * string;
+
+#endif
