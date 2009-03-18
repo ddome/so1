be0e02750feebef62a7d3fb1eab54c6021171587
domedamian <domedamian@b83fa03a-0f66-11de-b559-cb2af582aa9e>
2009-03-18 23:27:34 +0000

    Subo lo que hice hoy.El codigo del server esta bastante enquilombado, es mas pseudo codigo que otra cosa. El problema es que sin una herramienta de comunicacion, se me ahce muy dificil avanzar. Maniana lo hablamos.

--- Server/App/Application.h
+++ Server/App/Application.h
@@ -2,6 +2,28 @@
 #ifndef __APPLICATION_H__
 #define __APPLICATION_H__
 
+#include "Prompt.h"
+#include "../Lib/genlib.h"
+#include "../Lib/defines.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+//Por default el id del servidor prompt es 0
+#define PROMPT_ID 0
+
+//operaciones
+#define EXIT		0
+#define NEW_CLIENT  1
+#define DIR_REQ		2 // Es atendida por un proceso dedicado
+#define FILE_ADD	3
+#define FILE_REM    4
+#define FILE_ACT	5  // Es atendida por un proceso dedicado
+#define DIR_LIST    6
+#define TOP_REQ     7
+#define TOP_USR_REQ 8
+#define PING		9 // Envia para verificar coneccion
+
 void StartApplication(void);
 
 #endif
