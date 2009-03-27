35ffcd6bdb3390e8b8959f438537fd0edf2073cb
domedamian <domedamian@b83fa03a-0f66-11de-b559-cb2af582aa9e>
2009-03-27 14:46:39 +0000

    Header de cliente agregado, faltaria implementarlas....

--- Client/App/Application.h
+++ Client/App/Application.h
@@ -1,23 +1,62 @@
-/*
- *  client.h
- *  tpSO1
- *
- *  Created by Damian Dome on 3/15/09.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
- */
-#ifndef _CLIENT_H_
-#define _CLIENT_H_
 
-#include <unistd.h>
-#include "fileHandler.h"
+#ifndef __APPLICATION_H__
+#define __APPLICATION_H__
+
+#include "Prompt.h"
 #include "../Lib/genlib.h"
 #include "../Lib/defines.h"
-#include "Prompt.h"
+#include "fileHandler.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define SERVER_PATH "/bomsyinc/backup"
+#include "../sqliteADT/sqliteADT.h"
+#include "../Lib/pqADT.h"
+#include "DBexec.h"
 
 
-fileT * InitFiles(void);
-int ClientPrompt(void);
-int InitClientApp(void);
+/*---------------------------------------------------------------- 
+ Inicia la aplicación. Requerido antes de usar cualquier función
+ de la misma.
+ ----------------------------------------------------------------*/
+int InitApplication(void);
+
+/*---------------------------------------------------------------- 
+ Agrega un directorio 'dir', que contiene la lista de archivos
+ 'filesList, con su contenido binario almacenado en la posición
+ correspondiente de data.
+ ----------------------------------------------------------------*/
+int DirAdd( string dir, fileT *filesList, byte **data  );
+
+/*---------------------------------------------------------------- 
+ Borra un archivo del cliente
+ ----------------------------------------------------------------*/
+int FileRem( fileT file );
+
+/*---------------------------------------------------------------- 
+ Agrega un archivo al cliente. Recibe en data el arreglo de bytes
+ con el contenido binario del archivo. El usuario es responsable
+ de liberar data.
+ ----------------------------------------------------------------*/
+int FileAdd( fileT file, byte *data );
+
+/*---------------------------------------------------------------- 
+ Almacena en 'data' el contenido binario de un archivo indicado
+ por 'file'. El usuario es responsable de liberar data.
+ ----------------------------------------------------------------*/
+int FileReq( fileT file, byte *data );
+
+/*---------------------------------------------------------------- 
+ Retorna un arreglo de strings por cada directorio que utiliza 
+ el cliente. Devuelve NULL en caso de error o de estar vacia.
+ ----------------------------------------------------------------*/
+string * DirList(void);
+
+/*---------------------------------------------------------------- 
+ Vigila un directorio hasta que haya un cambio, devolviendo
+ un descriptor con el tipo de operación sobre el directorio.
+ ----------------------------------------------------------------*/
+int DirWatch(string dir);
 
 #endif
--- Server/App/Application.h
+++ Server/App/Application.h
@@ -10,22 +10,81 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#define SERVER_PATH "~/bomsyinc/backup"
+#define SERVER_PATH "/bomsyinc/backup"
 #include "../sqliteADT/sqliteADT.h"
 #include "../Lib/pqADT.h"
 #include "DBexec.h"
 
 
-int NewClient(string name);
-int ListDirs( const char * userName,string **out);
-int TopList(string *out);
-int TopListUser(int userID, string *out);
+/*---------------------------------------------------------------- 
+ Inicia la aplicación. Requerido antes de usar cualquier función
+ de la misma.
+ ----------------------------------------------------------------*/
 int InitApplication(void);
+
+/*---------------------------------------------------------------- 
+ Pregunta por el cliente. En caso de ser existente, lo loguea.
+ en caso de no existir, lo agrega a la base. En caso de existir y 
+ estar conectado, devuelve un error.
+ ----------------------------------------------------------------*/
+int NewClient(string name);
+
+/*---------------------------------------------------------------- 
+ Recibe un nombre de usuario y un puntero a arreglo de strings.
+ Devuelve la cantidad de directorios encontrados
+ o ERROR en caso de algun error.
+ Reserva en 'out' cada uno de los directorios registrados en el
+ server. El usuario debe encargarse de liberar out.
+----------------------------------------------------------------*/
+int ListDirs( const string userName,string **out);
+
+/*---------------------------------------------------------------- 
+ Devuelve la cantidad de acciones realizadas por los usuarios o 
+ ERROR en caso de algun error.
+ Reserva en out las 10 últimas acciones de los usuarios, incluido
+ el nombre de estos.
+ ----------------------------------------------------------------*/
+int TopList(string *out);
+
+/*---------------------------------------------------------------- 
+ Devuelve la cantidad de acciones realizadas por los usuarios o 
+ ERROR en caso de algun error.
+ Reserva en out las 10 últimas acciones del usuario userID
+ ----------------------------------------------------------------*/
+int TopListUser(int userID, string *out);
+
+/*---------------------------------------------------------------- 
+ Recibe un fileT y devuelve un arreglo de bytes con el contenido
+ binario de ese archivo. El tamaño del arreglo puedo consultarse
+ con GetSize(file).
+ ----------------------------------------------------------------*/
 byte* ReqFile( fileT file );
-int DirAdd( string dirName  );
+
+/*---------------------------------------------------------------- 
+ Agrega un directorio existente en el Filesystem del servidor, 
+ al repositorio del mismo, quedando registrado en la base de datos
+ ----------------------------------------------------------------*/
+int DirAdd( string dir  );
+
+/*---------------------------------------------------------------- 
+ Borra un archivo del servidor
+ ----------------------------------------------------------------*/
 int FileRem( fileT file );
+
+/*---------------------------------------------------------------- 
+ Agrega un archivo al servidor. Recibe en data el arreglo de bytes
+ con el contenido binario del archivo. El usuario es responsable
+ de liberar data.
+ ----------------------------------------------------------------*/
 int FileAdd( fileT file, byte *data );
 
+/*---------------------------------------------------------------- 
+ Guarda de manera lineal, los files correspondientes a un
+ directorio 'dir', con su respectiva estructura binaria reservado
+ en databuffer. El usuario es responsable de liberar files y
+ databuffer.
+ La función devuelve la cantidad de archivos leidos.
+ ----------------------------------------------------------------*/
 int ReqDir( string userName, string dir, fileT **files, byte ***databuffer );
 
 
