f0947375a50bbc7cf0f68ba3d6b8e5e3574ae0fd
domedamian <domedamian@b83fa03a-0f66-11de-b559-cb2af582aa9e>
2009-04-01 22:20:36 +0000

    Session Server acepta paquetes de manejo de archivos

--- Server/main.c
+++ Server/main.c
@@ -32,7 +32,32 @@ main(void)
 	 */
 	InitApplication();
 	
+	session_t aux;
+	fileT filein,fileout;
+	int pos;
+	byte *fileData;
+	byte *sessionData;
+	
+	strcpy(aux.msg,"Damian");
+	aux.pid = 31;
+	aux.opCode = CL_FIL_REM;
+	
+	filein = NewFileT("/SO","Prueba1");
+	fileData = ReqFile(filein);
+	
+	fileout = NewFileT("/OUT","Prueba1");
+	fileout.sb.st_size = filein.sb.st_size;
 	
 	
+	aux.dataSize = GetSize(fileout) + sizeof(fileT);		
+	aux.data = malloc(aux.dataSize);	
+	pos = 0;	
+	memmove(aux.data+pos, &fileout, sizeof(fileT));
+	pos += sizeof(fileT);
+	memmove(aux.data+pos, fileData, GetSize(fileout));
+
+	sessionData = MakeSessionData(aux);
+	ProcessRequest(&sessionData, 31);
+
     return 0;
 }	
