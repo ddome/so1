13e14f3ef962e4c2bca8b3e19b89df22e4f6a46a
elsilmaril <elsilmaril@b83fa03a-0f66-11de-b559-cb2af582aa9e>
2009-04-02 16:45:12 +0000

    Se inicio el servidor del cliente, agregando tambien la capa de transporte para FIFOS, con los path de lectura y escritura invertidos para comunicarse con el servidor.

---
+++ trunk/Client/App/Server.h
@@ -0,0 +1,48 @@
+
+/* Archivo: Server.h
+*  -----------------
+*  Interface Server.h, la cual maneja la creacion,
+*  continuidad y comportamiento de procesos.
+*/
+
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+/*
+*  System Includes
+*/
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+/*
+*  Project Includes
+*/
+
+#include "Prompt.h"
+#include "Application.h"
+#include "../Lib/defines.h"
+//#include "../Session/Session.h"
+
+/*
+*  Defines
+*/
+
+#define __DEFAULT_PID__	  0
+
+/* Tipos de procesos disponibles para crearse
+*/
+
+#define __SPAWN_PROMPT__  1
+#define __SPAWN_DIR__     2
+#define __SPAWN_DEMAND__  3
+
+/* Definiciones para creacion de procesos con fork()
+*/
+
+#define __ISCHILD__	      0
+#define __SHUT_DOWN__	 -2
